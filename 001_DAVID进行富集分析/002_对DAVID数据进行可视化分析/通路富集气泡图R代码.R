8bc631de42af33c561fc4a72ba607e4bb4f9bc9e
素质教育漏网之鱼 <102901955+Lantary@users.noreply.github.com>
2022-05-03 19:33:45 +0800

    Update 通路富集气泡图R代码.R

--- 001_DAVID进行富集分析/002_对DAVID数据进行可视化分析/通路富集气泡图R代码.R
+++ 001_DAVID进行富集分析/002_对DAVID数据进行可视化分析/通路富集气泡图R代码.R
@@ -13,18 +13,18 @@ keggSig = separate(keggSig, Term, sep = ":",
                    into = c("ID", "Term"))
 
 # 作图函数
-ggplot(keggSig, aes(x = Fold.Enrichment, y = Term))
-  +geom_point(aes(size = Count, color = -1*log10(PValue)))
-  +scale_colour_gradient(low = "green", high = "red")
-  +labs(
+ggplot(keggSig, aes(x = Fold.Enrichment, y = Term)) +
+  geom_point(aes(size = Count, color = -1*log10(PValue))) +
+  scale_colour_gradient(low = "green", high = "red") +
+  labs(
     color = expression(-log[10](P.value)),
     size = "Gene number",
     x = "Fold enrichment",
     y = "Pathway name",
     title = "Pathway enrichment"
-  )
-  +theme_bw()
-  +theme(
+  ) +
+  theme_bw() +
+  theme(
     axis.text.y = element_text(size = rel(1.3)),
     axis.title.x = element_text(size = rel(1.3)),
     axis.title.y = element_blank()
